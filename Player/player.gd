90cbc92fa16af237cd9a2123f72ba4b517ae834c
Aaron Kummer <aaronjkummer@gmail.com>
2023-03-29 09:37:47 -0400

    smoothes movement

--- Player/player.gd
+++ Player/player.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
+
+const ACCELERATION = 25 
 const MAX_SPEED  = 100
+const FRICTION = 10 
 
 var velocity = Vector2.ZERO
 
@@ -9,9 +12,10 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * (ACCELERATION * delta)
+		velocity = velocity.clamped(MAX_SPEED * delta)
 	else: 
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION*delta)
 		
-	move_and_collide(velocity * MAX_SPEED * delta)
+	move_and_collide(velocity)
 
